b8ea5f5df22cd59afbfb3432ae394d9d6d99d786
Ma7eer <maher.alkendi2@gmail.com>
2018-10-10 06:25:53 -0600

    added labels to the html file

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-
+const form = document.querySelector("form");
 // document.querySelector('button').addEventListener('click', function() {
 //   var doc = new jsPDF()
 
 // doc.text('Hello world!', 10, 10)
 // doc.save('a4.pdf')
 // })
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("works");
+})
