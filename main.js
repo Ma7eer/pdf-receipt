656e12e831f68e3265bed05439a0b89985822b9a
Ma7eer <maher.alkendi2@gmail.com>
2018-10-11 08:18:37 -0600

    ja file can now input into pdf file

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
-const form = document.querySelector("form");
-// document.querySelector('button').addEventListener('click', function() {
-//   var doc = new jsPDF()
-
-// doc.text('Hello world!', 10, 10)
-// doc.save('a4.pdf')
-// })
+const form = document.querySelector('form');
+const nameInput = document.getElementById('item-name');
+const dateInput = document.getElementById('date');
+const idInput = document.getElementById('payment-id');
+const methodInput = document.getElementById('payment-method');
+const paymentInput = document.getElementById('amount');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("works");
+  const doc = new jsPDF();
+
+  doc.setFontSize(26);
+  doc.setTextColor(92, 76, 76);
+  doc.text(20, 20, 'Transaction Details');
+  doc.setLineWidth(0.5);
+  doc.setDrawColor(214,214,214);
+  doc.line(20, 30, 180, 30);
+  doc.setFontSize(18);
+  doc.text(20, 45, `Name: ${name}`);
+  doc.text(20, 65, `Transaction Date: ${date}`);
+  doc.text(20, 85, `Transaction ID: ${id}`);
+  doc.text(20, 105, `Payment Method: ${method}`);
+  doc.text(20, 125, `Charged Amount: ${payment}`);
+
+  doc.save('receipt.pdf');
+})
+
+let name = nameInput.value;
+nameInput.addEventListener('input', (e) => {
+  name = e.target.value;
+})
+
+let date = dateInput.value;
+dateInput.addEventListener('input', (e) => {
+  date = e.target.value;
+})
+
+let id = idInput.value;
+idInput.addEventListener('input', (e) => {
+  id = e.target.value;
+})
+
+let method = methodInput.value;
+methodInput.addEventListener('input', (e) => {
+  method = e.target.value;
+})
+
+let payment = paymentInput.value;
+paymentInput.addEventListener('input', (e) => {
+  payment = e.target.value;
 })
